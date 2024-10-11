8a26c63f8de9770bee262c174881fccd72189514
Roman Romanov <roman_romanov2@epam.com>
2024-10-11 11:50:15 +0300

    add aiofiles parametrization for tests

--- noxfile.py
+++ noxfile.py
@@ -53,13 +53,17 @@ def format(session: nox.Session):
 @nox.parametrize("pydantic", ["1.10.17", "2.8.2"])
 @nox.parametrize("httpx", ["0.25.0", "0.27.0"])
 @nox.parametrize("openai", ["1.0.0", "1.51.0"])
-def test(session: nox.Session, pydantic: str, httpx: str, openai: str) -> None:
+@nox.parametrize("aiofiles", ["24.1.0", "22.1.0"])
+def test(
+    session: nox.Session, pydantic: str, httpx: str, openai: str, aiofiles: str
+) -> None:
     """Runs tests"""
     session.run("poetry", "install", external=True)
     session.install(
         f"pydantic=={pydantic}",
         f"httpx=={httpx}",
         f"openai=={openai}",
+        f"aiofiles=={aiofiles}",
     )
     session.run("pytest", "tests", "--ignore=tests/integration")
 
@@ -67,11 +71,15 @@ def test(session: nox.Session, pydantic: str, httpx: str, openai: str) -> None:
 @nox.session(python=["3.11"])
 @nox.parametrize("pydantic", ["1.10.17", "2.8.2"])
 @nox.parametrize("openai", ["1.0.0", "1.51.0"])
-def integration_test(session: nox.Session, pydantic: str, openai: str) -> None:
+@nox.parametrize("aiofiles", ["24.1.0", "22.1.0"])
+def integration_test(
+    session: nox.Session, pydantic: str, openai: str, aiofiles: str
+) -> None:
     """Run integration tests"""
     session.run("poetry", "install", external=True)
     session.install(
         f"pydantic=={pydantic}",
         f"openai=={openai}",
+        f"aiofiles=={aiofiles}",
     )
     session.run("pytest", "tests/integration")
