fbeb5656c6409c198de9d4a984a1b41ffcea0c42
Roman Romanov <roman_romanov2@epam.com>
2024-10-11 19:01:05 +0300

    update noxfile to test lower version of aiofiles

--- noxfile.py
+++ noxfile.py
@@ -53,7 +53,7 @@ def format(session: nox.Session):
 @nox.parametrize("pydantic", ["1.10.17", "2.8.2"])
 @nox.parametrize("httpx", ["0.25.0", "0.27.0"])
 @nox.parametrize("openai", ["1.0.0", "1.51.0"])
-@nox.parametrize("aiofiles", ["24.1.0", "0.5.0"])
+@nox.parametrize("aiofiles", ["0.5.0", "22.1.0", "24.1.0"])
 def test(
     session: nox.Session, pydantic: str, httpx: str, openai: str, aiofiles: str
 ) -> None:
