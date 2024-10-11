fe8218a8ffc6af322608ece864dc9b20782d6f3b
Roman Romanov <roman_romanov2@epam.com>
2024-10-11 14:31:47 +0300

    One more interface change for deployments - remove `get_raw`

--- aidial_client/resources/deployments.py
+++ aidial_client/resources/deployments.py
@@ -6,22 +6,22 @@ from aidial_client.types.deployment import Deployment, DeploymentsResponse
 
 
 class Deployments(Resource):
-    def raw_get(self) -> DeploymentsResponse:
+    def _list_raw(self) -> DeploymentsResponse:
         return self.http_client.request(
             cast_to=DeploymentsResponse,
             options=FinalRequestOptions(method="GET", url="openai/deployments"),
         )
 
     def list(self) -> List[Deployment]:
-        return self.raw_get().data
+        return self._list_raw().data
 
 
 class AsyncDeployments(AsyncResource):
-    async def raw_get(self) -> DeploymentsResponse:
+    async def _list_raw(self) -> DeploymentsResponse:
         return await self.http_client.request(
             cast_to=DeploymentsResponse,
             options=FinalRequestOptions(method="GET", url="openai/deployments"),
         )
 
     async def list(self) -> List[Deployment]:
-        return (await self.raw_get()).data
+        return (await self._list_raw()).data
