0b22a774dbf58bdb47e6aabfcf5f75f821e9c105
Roman Romanov <roman_romanov2@epam.com>
2024-10-11 20:06:18 +0300

    add test for metadata

--- tests/test_types.py
+++ tests/test_types.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic import ValidationError
 
 from aidial_client.types.chat.response import Attachment
+from aidial_client.types.metadata import BaseMetadata
 
 
 @pytest.mark.parametrize(
@@ -30,3 +31,26 @@ def test_valid_attachment(valid_attachment):
     attachment = Attachment(**valid_attachment)
     for key, value in valid_attachment.items():
         assert getattr(attachment, key) == value
+
+
+def test_metadata_population():
+    metadata_by_name = BaseMetadata(
+        name="test",
+        bucket="test",
+        url="test",
+        node_type="FOLDER",
+        resource_type="FILE",
+    )
+    alias_json = {
+        "name": "test",
+        "bucket": "test",
+        "url": "test",
+        "nodeType": "FOLDER",
+        "resourceType": "FILE",
+    }
+    metadata_by_alias = BaseMetadata(**alias_json)  # type: ignore
+
+    for field in BaseMetadata.__annotations__:
+        assert getattr(metadata_by_name, field) == getattr(
+            metadata_by_alias, field
+        )
