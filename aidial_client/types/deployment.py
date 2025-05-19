8ccfc308e9953a75bd772673c961e1386f2de7be
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2025-05-19 12:57:28 +0200

    Refactor application and deployment models for improved structure and clarity

--- aidial_client/types/deployment.py
+++ aidial_client/types/deployment.py
@@ -1,22 +1,43 @@
-from typing import List, Literal, Optional
+from typing import List, Literal, Optional, Dict
 
 from aidial_client._internal_types._model import ExtraAllowModel
-
 
 class ScaleSettings(ExtraAllowModel):
     scale_type: Literal["standard"]
 
+class Features(ExtraAllowModel):
+    rate: Optional[bool] = None
+    tokenize: Optional[bool] = None
+    temperature: Optional[bool] = None
+    truncate_prompt: Optional[bool] = None
+    configuration: Optional[bool] = None
+    system_prompt: Optional[bool] = None
+    tools: Optional[bool] = None
+    seed: Optional[bool] = None
+    url_attachments: Optional[bool] = None
+    folder_attachments: Optional[bool] = None
+    allow_resume: Optional[bool] = None
 
-class Deployment(ExtraAllowModel):
+class DialDeploymentBase(ExtraAllowModel):
     id: str
-    model: str
-    owner: str
-    object: Literal["deployment", "model"]
-    status: Literal["succeeded"]
+    object: str
+    owner: Optional[str] = None
+    reference: Optional[str] = None
+    icon_url: Optional[str] = None
+    description: Optional[str] = None
+    display_name: Optional[str] = None
+    display_version: Optional[str] = None
+    status: Optional[str] = None
     created_at: int
-    updated_at: int
+    updated_at: Optional[int] = None
     scale_settings: Optional[ScaleSettings] = None
+    defaults: Dict = {}
+    input_attachment_types: Optional[List[str]] = None
+    features: Optional[Features] = None
 
+class Deployment(DialDeploymentBase, ExtraAllowModel):
+    object: Literal["deployment", "model"]
+    model: str
 
 class DeploymentsResponse(ExtraAllowModel):
     data: List[Deployment]
