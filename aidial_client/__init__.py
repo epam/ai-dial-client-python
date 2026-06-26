ec301807eb732c535eaf30a9a6aa81d6ccb41271
Alex Chernyshov <alexfoxalt@gmail.com>
2026-06-26 12:56:37 +0300

    fix: export NotDialURLError from package root

--- aidial_client/__init__.py
+++ aidial_client/__init__.py
@@ -6,6 +6,7 @@ from aidial_client._exception import (
     EtagMismatchError,
     InvalidDialURLError,
     InvalidRequestError,
+    NotDialURLError,
     ParsingDataError,
     ResourceNotFoundError,
 )
@@ -25,6 +26,7 @@ __all__ = [
     "DialException",
     "InvalidDialURLError",
     "InvalidRequestError",
+    "NotDialURLError",
     "ParsingDataError",
     "EtagMismatchError",
     "ResourceNotFoundError",
